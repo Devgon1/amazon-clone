f22580d5ec90bdf5fe23724942290e905232a9c1
Devgon1 <grc74@hotmail.com>
2020-12-19 12:00:07 +0100

    commit

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import data from "../data";
 import Product from "../components/Product";
+
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products"); //by running this line, the array in backend will be transfered to data in the front end
+      setProducts(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="row center">
-      {data.products.map((product) => {
+      {products.map((product) => {
         return <Product key={product._id} {...product} />;
       })}
     </div>
